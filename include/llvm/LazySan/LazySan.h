d61325cf4b75c73a7a1c62d9a61209e9e4db5385
Jangseop Shin <jsshin@sor.snu.ac.kr>
2017-12-06 17:33:43 +0900

    initial impl

---
+++ include/llvm/LazySan/LazySan.h
@@ -0,0 +1,21 @@
+//===- LazySan.h -  -----------------------*- C++ -*-===//
+
+#ifndef LLVM_LAZYSAN_LAZYSAN_H
+#define LLVM_LAZYSAN_LAZYSAN_H
+
+#include "llvm/Pass.h"
+
+namespace llvm {
+  class LazySan : public ModulePass {
+  public:
+    static char ID;
+
+    LazySan() : ModulePass(ID) {}
+
+    bool runOnFunction(Function &F);
+    bool runOnModule(Module &M) override;
+    void getAnalysisUsage(AnalysisUsage &AU) const override;
+  };
+}
+
+#endif /* LLVM_LAZYSAN_LAZYSAN_H */
---
+++ lib/LazySan/LazySan.cpp
@@ -0,0 +1,131 @@
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/IR/InstVisitor.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Type.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/LazySan/LazySan.h"
+
+#define DEBUG_TYPE "lazy-san"
+
+using namespace llvm;
+
+namespace {
+  class LazySanVisitor : public InstVisitor<LazySanVisitor> {
+    SmallVector<AllocaInst *, 8> AllocaInsts;
+
+  public:
+    LazySanVisitor() {}
+
+    void visitAllocaInst(AllocaInst &I);
+    void visitStoreInst(StoreInst &I);
+    void visitReturnInst(ReturnInst &I);
+  };
+} // end anonymous namespace
+
+void LazySanVisitor::visitAllocaInst(AllocaInst &I) {
+  AllocaInsts.push_back(&I);
+}
+
+void LazySanVisitor::visitStoreInst(StoreInst &I) {
+  Value *Ptr = I.getValueOperand();
+  Type *Ty = Ptr->getType();
+  if (!Ty->isPointerTy())
+    return;
+
+  Function *DecRC = I.getModule()->getFunction("ls_dec_refcnt");
+  Function *IncRC = I.getModule()->getFunction("ls_inc_refcnt");
+
+  IRBuilder<> Builder(&I);
+
+  // decrease ref count
+  LoadInst *PtrBefore = Builder.CreateLoad(I.getPointerOperand());
+  Value *Cast = Builder.CreateBitCast(PtrBefore, Type::getInt8PtrTy(I.getContext()));
+  Builder.CreateCall(DecRC, {Cast});
+
+  // increase ref count
+  Cast = Builder.CreateBitCast(Ptr, Type::getInt8PtrTy(I.getContext()));
+  Builder.CreateCall(IncRC, {Cast});
+}
+
+void LazySanVisitor::visitReturnInst(ReturnInst &I) {
+  Function *DecRC = I.getModule()->getFunction("ls_dec_refcnt");
+
+  IRBuilder<> Builder(&I);
+
+  for (AllocaInst *AI : AllocaInsts) {
+    Type *Ty = AI->getAllocatedType();
+    if (Ty->isPointerTy()) {
+      LoadInst *Ptr = Builder.CreateLoad(AI);
+      Value *Cast = Builder.CreateBitCast(Ptr, Type::getInt8PtrTy(I.getContext()));
+      Builder.CreateCall(DecRC, {Cast});
+      continue;
+    }
+
+    if (Ty->isStructTy()) {
+      for (unsigned int i = 0, e = Ty->getStructNumElements(); i < e; ++i) {
+        Type *ElemTy = Ty->getStructElementType(i);
+        if (ElemTy->isPointerTy()) {
+          assert(0);
+          continue;
+        }
+
+        if (ElemTy->isArrayTy()) {
+          ElemTy = ElemTy->getArrayElementType();
+          assert(ElemTy->isIntegerTy());
+          continue;
+        }
+
+        assert(ElemTy->isIntegerTy());
+      }
+    }
+
+    if (Ty->isArrayTy()) {
+      Type *ElemTy = Ty->getArrayElementType();
+      assert(ElemTy->isIntegerTy());
+    }
+  }
+}
+
+char LazySan::ID = 0;
+
+static RegisterPass<LazySan> X("lazy-san", "Lazy Pointer Sanitizer Pass");
+
+bool LazySan::runOnFunction(Function &F) {
+  LazySanVisitor LSV;
+  LSV.visit(F);
+
+  // To force linking
+  if (F.getName().startswith("abcdef")) {
+    F.viewCFGOnly();
+    F.viewCFG();
+  }
+
+  return true;
+}
+
+bool LazySan::runOnModule(Module &M) {
+  LLVMContext &C = M.getContext();
+  M.getOrInsertFunction("ls_dec_refcnt",
+                        FunctionType::get(Type::getVoidTy(C),
+                                          {Type::getInt8PtrTy(C)}));
+  M.getOrInsertFunction("ls_inc_refcnt",
+                        FunctionType::get(Type::getVoidTy(C),
+                                          {Type::getInt8PtrTy(C)}));
+  dbgs() << "Hello World!!!\n";
+  for (Function &F : M.functions()) {
+    if (F.empty())
+      continue;
+
+    runOnFunction(F);
+  }
+
+  return true;
+}
+
+void LazySan::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.setPreservesCFG();
+}
